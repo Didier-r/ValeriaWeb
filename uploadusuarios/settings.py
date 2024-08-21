f03fd184366266c2ebdb08af276cb2148981e1c5
Didier <jhondidierreyesramirez@gmail.com>
2024-08-21 17:11:23 -0500

    fianl del trabajo o eso creo :)

--- uploadusuarios/settings.py
+++ uploadusuarios/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'uploadusuarios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'usuarios_valeria',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
