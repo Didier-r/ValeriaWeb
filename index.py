ed28c0e56dfa7a1faaf567b199fceea3bf94f061
Didier <jhondidierreyesramirez@gmail.com>
2024-09-13 21:53:26 -0500

    Muchos avanzes

--- index.py
+++ index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hola mundo'
+    return render_template('index.html')
+
+@app.route('/quienes-somos')
+def quienes_somos():
+    return render_template('quienesSomos.html')
+
+@app.route('/planes-entrenamiento')
+def planes_entrenamiento():
+    return render_template('planes_entrenamiento.html')
+
+@app.route('/clases-grupales')
+def clases_grupales():
+    return render_template('clases_grupales.html')
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run(debug=True)
